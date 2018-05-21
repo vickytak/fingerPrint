5b0fe8b2a182be5ec3e9c2b39948bee1637ec98d
vicky <vickykrk@hotmail.com>
2018-05-21 20:21:07 +0530

    fingerprint

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { FingerprintAIO, FingerprintOptions } from '@ionic-native/fingerprint-aio'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  fingerPrintOption: FingerprintOptions
+  constructor(public platform: Platform, private fingerprint: FingerprintAIO) {
+    this.fingerPrintOption ={
+      clientId:'fingerpring-demo',
+      clientSecret:'password',
+      disableBackup:true
+    }
   }
 
+  async showFingerPrint(){
+    try{
+     const available = await this.fingerprint.isAvailable();
+     console.log(available);
+    
+       const result = await this.fingerprint.show(this.fingerPrintOption);
+       console.log(result);
+    
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
 }
